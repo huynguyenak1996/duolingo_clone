f45359fe0b30d2958541e79005907fd46d9e82fb
nguyenhuyak <49011747+huynguyenak1996@users.noreply.github.com>
2024-04-07 21:54:43 +0700

    a

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -7,6 +7,11 @@ export default function Home() {
       <div className="relative w-[240px] h-[240px] lg:w-[424px] lg:h-[424px] mb-8 lg:mb-0">
         <Image src="/hero.png" fill alt="Hero" />
       </div>
+      <div className="flex flex-col items-center gap-y-8">
+        <h1 className="text-xl lg:text-3xl font-bold text-neutral-600 max-w-[480px] text-center">
+          Learn, practice and master new languages with VioLingo
+        </h1>
+      </div>
     </div>    
   )
 }
